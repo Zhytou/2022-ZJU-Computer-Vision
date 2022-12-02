027f48301a83455fb4e4dce1e424ce5c954579fb
zhytou <805007434@qq.com>
2022-12-02 19:44:23 +0800

    lab2

--- labs/lab2-stiching/main.cpp
+++ labs/lab2-stiching/main.cpp
@@ -1,9 +1,11 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 #include "opencv4/opencv2/opencv.hpp"
 
 #define KEY_POINT_NUM 500
+#define BEST_MATCH_POINT_NUM 10
 
 using namespace std;
 using namespace cv;
@@ -20,32 +22,31 @@ int main() {
   namedWindow(stichWindName, 0);
 
   // 拼接 5 组图片
-  for (int i = 1; i <= 5; i++) {
+  for (int imgIdx = 1; imgIdx <= 2; imgIdx++) {
     // 读取第 i 组图片
     char imgName[30];
-    sprintf(imgName, "../lab2-stiching/img/%d1.jpg", i);
+    sprintf(imgName, "../lab2-stiching/img/%d1.jpg", imgIdx);
     Mat img1 = imread(imgName);
     imgName[0] = '\0';
-    sprintf(imgName, "../lab2-stiching/img/%d2.jpg", i);
-    cout << imgName << endl;
+    sprintf(imgName, "../lab2-stiching/img/%d2.jpg", imgIdx);
     Mat img2 = imread(imgName);
 
     // SIFT特征点检测器
     Ptr<SIFT> detector = SIFT::create(KEY_POINT_NUM);
     // 特征点
-    vector<KeyPoint> keypoints1, keypoints2;
+    vector<KeyPoint> keyPoints1, keyPoints2;
 
     // 进行特征检测
-    detector->detect(img1, keypoints1);
-    detector->detect(img2, keypoints2);
+    detector->detect(img1, keyPoints1);
+    detector->detect(img2, keyPoints2);
 
     // 展示特征点检测结果
     Mat detectImg1;
-    drawKeypoints(img1, keypoints1, detectImg1);
+    drawKeypoints(img1, keyPoints1, detectImg1);
     imshow(detectWindName1, detectImg1);
 
     Mat detectImg2;
-    drawKeypoints(img2, keypoints2, detectImg2);
+    drawKeypoints(img2, keyPoints2, detectImg2);
     imshow(detectWindName2, detectImg2);
 
     // 特征点描述提取器
@@ -53,20 +54,47 @@ int main() {
 
     // 提取特征点描述符
     Mat describeImg1, describeImg2;
-    descriptor->compute(img1, keypoints1, describeImg1);
-    descriptor->compute(img2, keypoints2, describeImg2);
+    descriptor->compute(img1, keyPoints1, describeImg1);
+    descriptor->compute(img2, keyPoints2, describeImg2);
 
-    // 进行BFMatch暴力匹配
-    BFMatcher matcher(NORM_L2);
-    // 定义匹配结果变量
-    vector<DMatch> matches;
+    // 特征匹配器
+    BFMatcher matcher;
+    vector<DMatch> matchPoints;
     // 实现描述符之间的匹配
-    matcher.match(describeImg1, describeImg1, matches);
+    matcher.match(describeImg1, describeImg2, matchPoints);
 
+    // 展示特征匹配结果
     Mat matchImg;
-    drawMatches(img1, keypoints1, img2, keypoints2, matches, matchImg);
+    drawMatches(img1, keyPoints1, img2, keyPoints2, matchPoints, matchImg);
     imshow(matchWindName, matchImg);
 
+    // 找出图中匹配的最好的点
+    sort(matchPoints.begin(), matchPoints.end());
+    vector<Point2f> bestMatchPoints1, bestMatchPoints2;
+    for (int i = 0; i < BEST_MATCH_POINT_NUM; i++) {
+      bestMatchPoints1.push_back(keyPoints1[matchPoints[i].queryIdx].pt);
+      bestMatchPoints2.push_back(keyPoints2[matchPoints[i].trainIdx].pt);
+    }
+
+    // 计算映射矩阵
+    Mat transformMat =
+        findHomography(bestMatchPoints1, bestMatchPoints2, RANSAC);
+
+    // 计算匹配点，用于图像拼接定位
+
+    // 进行图片变换
+    Mat transformImg;
+    warpPerspective(img1, transformImg, transformMat,
+                    Size(img2.cols, img2.rows));
+
+    // 拼接后图像
+    int height = max(img1.rows, img2.rows), width = img1.cols + img2.cols;
+    Mat stichImg(height, width, CV_8UC3);
+    transformImg.copyTo(
+        stichImg(Rect(0, 0, transformImg.cols, transformImg.rows)));
+    img2.copyTo(stichImg(Rect(0, 0, img2.cols, img2.rows)));
+
+    imshow(stichWindName, stichImg);
     waitKey(0);
   }
 
