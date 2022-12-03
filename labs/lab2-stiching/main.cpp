b9885df4669aa8e64277d55cb1c204f232997616
zhytou <805007434@qq.com>
2022-12-03 20:12:42 +0800

    lab2 stiching

--- labs/lab2-stiching/main.cpp
+++ labs/lab2-stiching/main.cpp
@@ -5,7 +5,7 @@
 #include "opencv4/opencv2/opencv.hpp"
 
 #define KEY_POINT_NUM 500
-#define BEST_MATCH_POINT_NUM 10
+#define GOOD_MATCH_POINT_NUM 10
 
 using namespace std;
 using namespace cv;
@@ -16,19 +16,23 @@ int main() {
              detectWindName2[] = "lab2-detect-results2";
   const char matchWindName[] = "lab2-match-results";
   const char stichWindName[] = "lab2-stich-results";
+  const char blendWindName[] = "lab2-blend-results";
   namedWindow(detectWindName1, 0);
   namedWindow(detectWindName2, 0);
   namedWindow(matchWindName, 0);
   namedWindow(stichWindName, 0);
+  namedWindow(blendWindName, 0);
 
   // 拼接 5 组图片
-  for (int imgIdx = 1; imgIdx <= 2; imgIdx++) {
+  for (int imgIdx = 1; imgIdx <= 5; imgIdx++) {
     // 读取第 i 组图片
     char imgName[30];
     sprintf(imgName, "../lab2-stiching/img/%d1.jpg", imgIdx);
+    // 读取右图
     Mat img1 = imread(imgName);
     imgName[0] = '\0';
     sprintf(imgName, "../lab2-stiching/img/%d2.jpg", imgIdx);
+    // 读取左图
     Mat img2 = imread(imgName);
 
     // SIFT特征点检测器
@@ -70,31 +74,60 @@ int main() {
 
     // 找出图中匹配的最好的点
     sort(matchPoints.begin(), matchPoints.end());
-    vector<Point2f> bestMatchPoints1, bestMatchPoints2;
-    for (int i = 0; i < BEST_MATCH_POINT_NUM; i++) {
-      bestMatchPoints1.push_back(keyPoints1[matchPoints[i].queryIdx].pt);
-      bestMatchPoints2.push_back(keyPoints2[matchPoints[i].trainIdx].pt);
+    vector<Point2f> goodMatchPoints1, goodMatchPoints2;
+    for (int i = 0; i < GOOD_MATCH_POINT_NUM; i++) {
+      goodMatchPoints1.push_back(keyPoints1[matchPoints[i].queryIdx].pt);
+      goodMatchPoints2.push_back(keyPoints2[matchPoints[i].trainIdx].pt);
     }
 
     // 计算映射矩阵
-    Mat transformMat =
-        findHomography(bestMatchPoints1, bestMatchPoints2, RANSAC);
-
-    // 计算匹配点，用于图像拼接定位
+    Mat transformMat = findHomography(goodMatchPoints1, goodMatchPoints2);
 
     // 进行图片变换
-    Mat transformImg;
-    warpPerspective(img1, transformImg, transformMat,
-                    Size(img2.cols, img2.rows));
-
+    Mat newImg1;
+    warpPerspective(img1, newImg1, transformMat,
+                    Size(img1.cols + img2.cols, img1.rows));
     // 拼接后图像
-    int height = max(img1.rows, img2.rows), width = img1.cols + img2.cols;
-    Mat stichImg(height, width, CV_8UC3);
-    transformImg.copyTo(
-        stichImg(Rect(0, 0, transformImg.cols, transformImg.rows)));
+    Mat stichImg(newImg1);
     img2.copyTo(stichImg(Rect(0, 0, img2.cols, img2.rows)));
-
     imshow(stichWindName, stichImg);
+
+    // 图像融合
+    warpPerspective(img1, newImg1, transformMat,
+                    Size(img1.cols + img2.cols, img1.rows));
+    Mat blendImg(Size(img1.cols + img2.cols, img1.rows), CV_8UC3,
+                 Scalar(255, 255, 255));
+
+    // 计算重叠部分上下边界
+    vector<Point2f> sourceCornerPoints(4), targetCornerPoints(4);
+    sourceCornerPoints[0] = Point2f(0, 0);
+    sourceCornerPoints[1] = Point2f(img1.cols, 0);
+    sourceCornerPoints[2] = Point2f(img1.cols, img1.rows);
+    sourceCornerPoints[3] = Point2f(0, img1.rows);
+    perspectiveTransform(sourceCornerPoints, targetCornerPoints, transformMat);
+
+    int low = int(min(targetCornerPoints[0].x, targetCornerPoints[3].x));
+    int high = img2.cols;
+    int overlapWidth = high - low + 1;
+
+    cout << low << ' ' << high << endl;
+    cout << blendImg.cols << ' ' << blendImg.rows << endl;
+
+    // 加权融合
+    for (int i = 0; i < blendImg.rows; i++) {
+      for (int j = 0; j < low; j++) {
+        blendImg.at<Vec3b>(i, j) = img2.at<Vec3b>(i, j);
+      }
+      for (int j = low; j <= high; j++) {
+        double fraction = 1.0 * (j - low) / overlapWidth;
+        blendImg.at<Vec3b>(i, j) = fraction * newImg1.at<Vec3b>(i, j) +
+                                   (1 - fraction) * img2.at<Vec3b>(i, j);
+      }
+      for (int j = high + 1; j < blendImg.cols; j++) {
+        blendImg.at<Vec3b>(i, j) = newImg1.at<Vec3b>(i, j);
+      }
+    }
+    imshow(blendWindName, blendImg);
     waitKey(0);
   }
 
